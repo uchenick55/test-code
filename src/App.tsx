332f9a4141cf669d1bfaeea6118ebeaf95b0b41e
uchenick55 <uchenick55@yandex.ru>
2023-07-22 10:03:30 +0300

    вывел данные с сервера + React.memo и useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,93 @@
 // Мы ожидаем, что Вы исправите синтаксические ошибки, сделаете перехват возможных исключений и улучшите читаемость кода.
 // А так же, напишите кастомный хук useThrottle и используете его там где это нужно.
-// Желательно использование React.memo и React.useCallback там где это имеет смысл.
+//+ Желательно использование React.memo и React.useCallback там где это имеет смысл.
 // Будет большим плюсом, если Вы сможете закэшировать получение случайного пользователя.
-// Укажите правильные типы.
+//+ Укажите правильные типы.
 // По возможности пришлите Ваш вариант в https://codesandbox.io
 
-import React, {useState} from "react";
+import React, {useCallback, useState} from "react";
 
 const URL = "https://jsonplaceholder.typicode.com/users";
 
 type Company = {
-  bs: string;
-  catchPhrase: string;
-  name: string;
+    bs: string;
+    catchPhrase: string;
+    name: string;
 };
 
 type User = {
-  id: number;
-  email: string;
-  name: string;
-  phone: string;
-  username: string;
-  website: string;
-  company: Company;
-  address: any
+    id: number;
+    email: string;
+    name: string;
+    phone: string;
+    username: string;
+    website: string;
+    company: Company;
+    address: any
 };
 
-interface IButtonProps {
-  onClick: any;
+interface ButtonPropsType {
+    onClick: () => void;
 }
 
-const Button: React.FC<IButtonProps> = ({onClick}) =>  {
-  return (
-      <button type="button" onClick={onClick}>
-        get random user
-      </button>
-  );
+const Button: React.FC<ButtonPropsType> = React.memo( ({onClick}) => { // компонента кнопки, принимает обработчик нажатия на кнопку
+    console.log("Button")
+    return (
+        <button type="button" onClick={onClick}>
+            get random user
+        </button>
+    );
+})
+
+type UserInfoPropsType = {
+    user: User | null; // тип принимаемых данных
 }
 
-/*type UserInfoPropsType = {
-  user: User;
-}*/
-
-/*const UserInfo:React.FC<UserInfoPropsType> = ({ user }) => {
-  return (
-      <table>
-        <thead>
-        <tr>
-          <th>Username</th>
-          <th>Phone number</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>{user.name}</td>
-          <td>{user.phone}</td>
-        </tr>
-        </tbody>
-      </table>
-  );
-}*/
+const UserInfo: React.FC<UserInfoPropsType> = React.memo( ({user}) => { // компонента вывода данных по пользователю
+    console.log("UserInfo")
+    // вывод данных пользователя только если они не нулевые
+    return <div>
+        <table>
+            <thead>
+            <tr>
+                <th>Username</th>
+                <th>Phone number</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>{user?.name}</td>
+                <td>{user?.phone}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+})
 
 const App: React.FC = () => {
-  const [item, setItem] = useState<User | null> (null);
-  console.log(item)
+    console.log("App")
+    const [item, setItem] = useState<User | null>(null); // локальный стейт по пользователю (изначально занулен)
+    const [value, setValue] = useState("hello!")
+    const receiveRandomUser = async () => { // асинхронная функция получения случайного пользователя
+        const id = Math.floor(Math.random() * (10 - 1)) + 1; // случайное число от 1 до 10
+        const response = await fetch(`${URL}/${id}`); //получить ответ от сервера по пользователю со случайным номером
+        const user: User = await response.json(); // преобразовать ответ в json формат
+        user && setItem(user); // записать ответ в локальный стейт
+    };
 
-  const receiveRandomUser = async () => { // асинхронная функция получения случайного пользователя
-    const id = Math.floor(Math.random() * (10 - 1)) + 1; // случайное число от 1 до 10
-    const response = await fetch(`${URL}/${id}`); //получить ответ от сервера по пользователю со случайным номером
-    const _user: User  = await response.json(); // преобразовать ответ в json формат
-    setItem(_user); // записать ответ в локальный стейт
-  };
-
-  const handleButtonClick = () => { // обработчик нажатия на кнопку
-    receiveRandomUser(); // запустить получение случайного пользователя и запись в локальный стейт
-  };
-  return (
-      <div>
-        <header>Get a random user</header>
-        <Button onClick={handleButtonClick} /> {/*кнопка с обработчиком на получение случайного пользователя и запись в стейт*/}
-       {/* <UserInfo user={item} />*/}
-      </div>
-  );
+    const handleButtonClick = useCallback( () => { // обработчик нажатия на кнопку
+        receiveRandomUser(); // асинхронная функция получения случайного пользователя и запись в локальный стейт
+    },[])
+    return (
+        <div>
+            {/*заголовок*/}
+            <header>Get a random user</header>
+            <input value={value} onChange={(event)=>setValue(event.target.value)}/>
+            <Button
+                onClick={handleButtonClick}/> {/*кнопка с обработчиком на получение случайного пользователя и запись в стейт*/}
+            {<UserInfo user={item}/>} {/* вывод данных по пользователю только когда они не нулевые*/}
+        </div>
+    );
 }
 
 export default App;
