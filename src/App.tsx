695ffed34b5a3615adc033a3c7817498d7fb6006
uchenick55 <uchenick55@yandex.ru>
2023-07-22 17:16:27 +0300

    добавил комментарии, немного сменил логику disable кнопки

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ import React, {useCallback, useState, useEffect} from "react";
 
 const URL = "https://jsonplaceholder.typicode.com/users";
 
-type Company = {
+type Company = { // тип компании
     bs: string;
     catchPhrase: string;
     name: string;
 };
 
-type User = {
+type User = { // тип пользователя
     id: number;
     email: string;
     name: string;
@@ -26,13 +26,13 @@ type User = {
     address: any
 };
 
-interface ButtonPropsType {
+type ButtonPropsType = {
     disabled: boolean
     onClick: () => void;
 }
 
-const Button: React.FC<ButtonPropsType> = React.memo(({onClick, disabled}) => { // компонента кнопки, принимает обработчик нажатия на кнопку
-    console.log("Button")
+const Button: React.FC<ButtonPropsType> = React.memo(({onClick, disabled}) => { // компонента кнопки, принимает обработчик нажатия на кнопку и маркер disabled
+    console.log("Button") // проверка количества ререндеров компоненты
     return (
         <button type="button" onClick={onClick} disabled={disabled}>
             get random user
@@ -41,12 +41,12 @@ const Button: React.FC<ButtonPropsType> = React.memo(({onClick, disabled}) => { 
 })
 
 type UserInfoPropsType = {
-    user: User | null; // тип принимаемых данных
+    user: User; // тип принимаемых пользователей
 }
 
 const UserInfo: React.FC<UserInfoPropsType> = React.memo(({user}) => { // компонента вывода данных по пользователю
-    console.log("UserInfo")
-    // вывод данных пользователя только если они не нулевые
+    console.log("UserInfo") // проверка количества ререндеров компоненты
+    const {name, phone} = user // получение данных пользователя из пропсов и последующая отрисовка
     return <div>
         <table>
             <thead>
@@ -57,46 +57,50 @@ const UserInfo: React.FC<UserInfoPropsType> = React.memo(({user}) => { // ком
             </thead>
             <tbody>
             <tr>
-                <td>{user?.name}</td>
-                <td>{user?.phone}</td>
+                <td>{name}</td>
+                <td>{phone}</td>
             </tr>
             </tbody>
         </table>
     </div>
 })
 const App: React.FC = () => {
-    console.log("App")
+    console.log("App") // проверка количества ререндеров компоненты
     const [item, setItem] = useState<User | null>(null); // локальный стейт по пользователю (изначально занулен)
-    const [value, setValue] = useState("hello!")
-    const [isFetching, setIsFetching] = useState(false)
+    const [isFetching, setIsFetching] = useState(false) // флаг загрузки (для disable кнопки)
 
-    useEffect(()=>{
-        if (isFetching) {
-            setTimeout(()=>{
-                setIsFetching(false)
+    useEffect(() => {
+        if (isFetching) { // если кнопка нажата
+            const clearId = setTimeout(() => { // задержка для disable кнопки
+                receiveRandomUser(); // асинхронная функция получения случайного пользователя и запись в локальный стейт
             }, 500)
+            return () => {
+                clearTimeout(clearId) // зачистка утечки памяти
+            }
         }
-    },[isFetching])
+    }, [isFetching])
 
     const receiveRandomUser = async () => { // асинхронная функция получения случайного пользователя
         const id = Math.floor(Math.random() * (10 - 1)) + 1; // случайное число от 1 до 10
         const response = await fetch(`${URL}/${id}`); //получить ответ от сервера по пользователю со случайным номером
         const user: User = await response.json(); // преобразовать ответ в json формат
-        user && setItem(user); // записать ответ в локальный стейт
+        if (user) {
+            setItem(user)
+            setIsFetching(false) // после disable с кнопки снимается
+        }
+        ;  // если пришли данные по пользователю с сервера, записать ответ в локальный стейт
     };
 
-    const handleButtonClick = useCallback(() => { // обработчик нажатия на кнопку
-        console.log("==================================")
-        receiveRandomUser(); // асинхронная функция получения случайного пользователя и запись в локальный стейт
-        setIsFetching(true)
+    const handleButtonClick = useCallback(() => { // обработчик нажатия на кнопку, обернули в useCallback чтобы не рисовалась лишний раз Button при возможных ререндаерах родителя
+        console.log("==================================") // начало цикла ререндера при нажатии на кнопку
+        setIsFetching(true) // задать флаг загрузки в true для disable кнопки
     }, [])
     return (
         <div>
             {/*заголовок*/}
             <header>Get a random user</header>
-            <input value={value} onChange={(event) => setValue(event.target.value)}/>
             <Button
-                disabled = {isFetching}
+                disabled={isFetching} // флаг для disable кнопки в момент загрузки и задержки между загрузками 500мс
                 onClick={handleButtonClick}/> {/*кнопка с обработчиком на получение случайного пользователя и запись в стейт*/}
             {item && <UserInfo user={item}/>} {/* вывод данных по пользователю только когда они не нулевые*/}
         </div>
